bf00f942600eafd1fff6f56c22d3856c270d368d
8ctopus <hello@octopuslabs.io>
2023-03-22 10:09:54 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Subs for php-spx
+
+## Installation
+
+      composer require --dev 8ctopus/php-spx-subs
---
+++ src/subs.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+function spx_profiler_start() : void
+{
+}
+
+function spx_profiler_stop() : void
+{
+}
+
+function spx_profiler_full_report_set_custom_metadata_str(string $customMetadataStr) : void
+{
+}
